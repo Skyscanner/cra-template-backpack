6c916acf4afd12ca0e60f3e7d51d747214a333c9
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-03-27 10:33:04 +0000

    Initial commit

---
+++ template/src/index.js
@@ -0,0 +1,8 @@
+import 'bpk-stylesheets/base';
+import 'bpk-stylesheets/base.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
