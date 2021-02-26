ef3e1eaa2b408eb79ab879d2ac493851ba27d013
Oliver Curtis <oliver.curtis@skyscanner.net>
2021-02-26 14:55:58 +0000

    [NO-JIRA]: Updating template

--- template/src/index.js
+++ template/src/index.js
@@ -1,5 +1,7 @@
 import 'bpk-stylesheets/base';
 import 'bpk-stylesheets/base.css';
+import 'bpk-stylesheets/font';
+import 'bpk-stylesheets/font.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
